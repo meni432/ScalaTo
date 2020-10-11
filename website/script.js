77c80886b821f9c32101cf8f07daf60d104c6461
Meni Samet <meni@carbyne911.com>
2020-10-11 03:01:02 +0300

    add website

---
+++ website/script.js
@@ -0,0 +1,25 @@
+async function postData(url, data) {
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    console.log(response)
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+  
+
+  const inputCode = document.getElementById('inputCode');
+  const outputCode = document.getElementById('outputCode');
+
+async function convert() {
+    console.log("before send "+ inputCode.value)
+    return postData(' https://h63frmmax9.execute-api.us-east-1.amazonaws.com/dev/ScalaToJavaConverter', { scalaCode: inputCode.value })
+    .then(data => {
+      console.log(data); 
+      outputCode.value = data.javaCode;
+    });
+}
